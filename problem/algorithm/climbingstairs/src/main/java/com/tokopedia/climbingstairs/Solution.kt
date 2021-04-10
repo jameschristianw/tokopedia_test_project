6c8f5d91bce2218fd3bebda64e0cc678afe26409
James Christian Wira <jamesch888@gmail.com>
2021-04-11 00:02:59 +0700

    Finish Climbing Stairs Problem

--- problem/algorithm/climbingstairs/src/main/java/com/tokopedia/climbingstairs/Solution.kt
+++ problem/algorithm/climbingstairs/src/main/java/com/tokopedia/climbingstairs/Solution.kt
@@ -1,10 +1,25 @@
 package com.tokopedia.climbingstairs
 
+import android.util.Log
+import java.lang.Math.pow
+import kotlin.math.round
+import kotlin.math.sqrt
+
 object Solution {
     fun climbStairs(n: Int): Long {
         // TODO, return in how many distinct ways can you climb to the top. Each time you can either climb 1 or 2 steps.
         // 1 <= n < 90
 
-        return 0
+        val result = fib(n+1).toLong()
+        Log.d("ClimbingStairsSolution", result.toString())
+
+        return result
     }
+
+    private fun fib(n: Int) : Int {
+        if (n <= 1) return n
+
+        return fib(n-1) + fib(n-2)
+    }
+
 }
