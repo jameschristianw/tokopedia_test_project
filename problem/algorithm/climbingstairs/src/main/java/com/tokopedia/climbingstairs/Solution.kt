f31dfecd799aa4ef15a910068b4d728c51b7dd19
James Christian Wira <jamesch888@gmail.com>
2021-04-11 18:50:12 +0700

    Changing fibonacci algorithm

--- problem/algorithm/climbingstairs/src/main/java/com/tokopedia/climbingstairs/Solution.kt
+++ problem/algorithm/climbingstairs/src/main/java/com/tokopedia/climbingstairs/Solution.kt
@@ -10,16 +10,26 @@ object Solution {
         // TODO, return in how many distinct ways can you climb to the top. Each time you can either climb 1 or 2 steps.
         // 1 <= n < 90
 
-        val result = fib(n+1).toLong()
+        val result = fib((n+1).toLong())
         Log.d("ClimbingStairsSolution", result.toString())
 
         return result
     }
 
-    private fun fib(n: Int) : Int {
-        if (n <= 1) return n
+    private fun fib(n: Long) : Long {
+        var a = 0L
+        var b = 1L
+        var c = 1L
+        if (n == 0L) return a
 
-        return fib(n-1) + fib(n-2)
+        for(i in 2..n){
+            println(i)
+            c = a + b
+            a = b
+            b = c
+        }
+
+        return b
     }
 
 }
